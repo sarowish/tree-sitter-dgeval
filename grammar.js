5607a147d4213002285e44386b16d5fd47ccfaf1
Berke Enercan <berkeenercan@tutanota.com>
2025-05-30 00:57:23 +0300

    style: format with prettier

--- grammar.js
+++ grammar.js
@@ -17,108 +17,100 @@ const PREC = {
   ternary: 2,
   assign: 1,
   comma: 0,
-}
+};
 
 module.exports = grammar({
   name: "dgeval",
 
   rules: {
-    source_file: $ => repeat($._statement),
+    source_file: ($) => repeat($._statement),
 
-    _statement: $ => choice(
-      $.expression_statement,
-      $.wait_statement,
-    ),
+    _statement: ($) => choice($.expression_statement, $.wait_statement),
 
-    expression_statement: $ => seq(
-      $._expression,
-      ';'
-    ),
+    expression_statement: ($) => seq($._expression, ";"),
 
-    wait_statement: $ => seq(
-      'wait',
-      field('identifiers', sepComma($.identifier)),
-      'then',
-      $._expression,
-      ';'
-    ),
+    wait_statement: ($) =>
+      seq(
+        "wait",
+        field("identifiers", sepComma($.identifier)),
+        "then",
+        $._expression,
+        ";",
+      ),
 
-    _expression: $ => choice(
-      $.identifier,
-      $.number_literal,
-      $.boolean_literal,
-      $.string_literal,
-      $.ternary_expression,
-      $.binary_expression,
-      $.unary_expression,
-      $.call_expression,
-      $.array,
-      $.assignment_expression,
-      seq('(', $._expression, ')'),
-      prec.left(PREC.comma, seq($._expression, ',', $._expression)),
-    ),
+    _expression: ($) =>
+      choice(
+        $.identifier,
+        $.number_literal,
+        $.boolean_literal,
+        $.string_literal,
+        $.ternary_expression,
+        $.binary_expression,
+        $.unary_expression,
+        $.call_expression,
+        $.array,
+        $.assignment_expression,
+        seq("(", $._expression, ")"),
+        prec.left(PREC.comma, seq($._expression, ",", $._expression)),
+      ),
 
-    ternary_expression: $ => prec.left(
-      PREC.ternary,
-      seq($._expression, '?', $._expression, ':', $._expression)
-    ),
+    ternary_expression: ($) =>
+      prec.left(
+        PREC.ternary,
+        seq($._expression, "?", $._expression, ":", $._expression),
+      ),
 
-    binary_expression: $ => {
+    binary_expression: ($) => {
       const table = [
-        [PREC.logical, choice('&&', '||')],
-        [PREC.comparative, choice('==', '!=', '<', '>', '<=', '>=')],
-        [PREC.additive, choice('+', '-')],
-        [PREC.multiplicative, choice('*', '/')],
+        [PREC.logical, choice("&&", "||")],
+        [PREC.comparative, choice("==", "!=", "<", ">", "<=", ">=")],
+        [PREC.additive, choice("+", "-")],
+        [PREC.multiplicative, choice("*", "/")],
       ];
 
-      return choice(...table.map(([precedence, operator]) => prec.left(precedence, seq(
-        $._expression,
-        field("operator", operator),
-        $._expression,
-      ))));
+      return choice(
+        ...table.map(([precedence, operator]) =>
+          prec.left(
+            precedence,
+            seq($._expression, field("operator", operator), $._expression),
+          ),
+        ),
+      );
     },
 
-    unary_expression: $ => choice(
-      prec.right(PREC.unary, seq('!', $._expression)),
-      prec.right(PREC.unary, seq('-', $._expression)),
-    ),
+    unary_expression: ($) =>
+      choice(
+        prec.right(PREC.unary, seq("!", $._expression)),
+        prec.right(PREC.unary, seq("-", $._expression)),
+      ),
 
-
-    call_expression: $ => prec.left(PREC.call, seq(
-      field("name", $._expression),
-      '(',
-      optional($._expression),
-      ')'
-    )),
+    call_expression: ($) =>
+      prec.left(
+        PREC.call,
+        seq(field("name", $._expression), "(", optional($._expression), ")"),
+      ),
 
     // captures both array literals and array access
-    array: $ => seq(
-      optional($._expression),
-      '[',
-      $._expression,
-      ']'
-    ),
+    array: ($) => seq(optional($._expression), "[", $._expression, "]"),
 
-    assignment_expression: $ => prec.right(
-      PREC.assign,
-      seq($._expression, '=', $._expression)
-    ),
+    assignment_expression: ($) =>
+      prec.right(PREC.assign, seq($._expression, "=", $._expression)),
 
-    number_literal: _ => {
+    number_literal: (_) => {
       const decimalDigits = /\d+/;
       const nonzeroInteger = seq(/[1-9]/, optional(decimalDigits));
-      const integer = choice('0', nonzeroInteger);
-      const fractional = seq('.', repeat(decimalDigits), repeat1(/[1-9]/));
+      const integer = choice("0", nonzeroInteger);
+      const fractional = seq(".", repeat(decimalDigits), repeat1(/[1-9]/));
 
       const significand = choice(
         seq(integer, optional(fractional)),
-        fractional
-      )
+        fractional,
+      );
 
       const exponent = seq(
-        choice('e', 'E'),
-        optional(choice('-', '+')),
-        nonzeroInteger
+        choice("e", "E"),
+        optional(choice("-", "+")),
+        nonzeroInteger,
       );
 
       const number = seq(significand, optional(exponent));
@@ -126,32 +118,18 @@ module.exports = grammar({
       return token(number);
     },
 
-    boolean_literal: _ => choice('true', 'false'),
+    boolean_literal: (_) => choice("true", "false"),
 
-    string_literal: $ => seq(
-      '"',
-      repeat(choice(
-        /[^\\"\n]/,
-        $.escape_sequence,
-      )),
-      '"',
-    ),
+    string_literal: ($) =>
+      seq('"', repeat(choice(/[^\\"\n]/, $.escape_sequence)), '"'),
 
-    escape_sequence: _ => token.immediate(seq(
-      '\\',
-      choice(
-        /x[0-9a-fA-F]{1,2}/,
-        /[n\\"]/
-      )
-    )),
+    escape_sequence: (_) =>
+      token.immediate(seq("\\", choice(/x[0-9a-fA-F]{1,2}/, /[n\\"]/))),
 
-    identifier: _ => seq(
-      /[a-z-A-z_]/,
-      repeat(/[a-zA-Z0-9_]/)
-    ),
-  }
+    identifier: (_) => seq(/[a-z-A-z_]/, repeat(/[a-zA-Z0-9_]/)),
+  },
 });
 
 function sepComma(rule) {
-  return seq(rule, repeat(seq(',', rule)));
+  return seq(rule, repeat(seq(",", rule)));
 }
